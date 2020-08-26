cfb9e249f50adb71ac8b16f824cdeb79a9d3fdce
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-26 14:26:52 +0700

    Initial commit

---
+++ src/com/clonexy/bedwars/Main.java
@@ -0,0 +1,18 @@
+package com.clonexy.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private String prefix = "§7[§dBedWars§7] §r";
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
